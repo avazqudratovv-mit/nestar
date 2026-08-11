359c9d46e127867bbc2fb6bc520b36f406c8c9a2
Kenneth <avazqudratovv@gmail.com>
2026-08-11 16:00:54 -0700

    feat: configure env variables

--- apps/nestar-batch/src/main.ts
+++ apps/nestar-batch/src/main.ts
@@ -1,8 +1,9 @@
 import { NestFactory } from '@nestjs/core';
 import { NestarBatchModule } from './nestar-batch.module';
 
+
 async function bootstrap() {
   const app = await NestFactory.create(NestarBatchModule);
-  await app.listen(process.env.port ?? 3000);
+  await app.listen(process.env.PORT_BATCH ?? 3000);
 }
 bootstrap();
