b6d1d2e9272cda0969669ae7dc8c477e38930b08
Kenneth <avazqudratovv@gmail.com>
2026-08-26 04:44:35 -0700

    feat: develop getMember GraphQL api

--- apps/nestar-api/src/components/member/member.resolver.ts
+++ apps/nestar-api/src/components/member/member.resolver.ts
@@ -9,6 +9,8 @@ import { ObjectId } from 'mongoose';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { MemberType } from '../../libs/enums/member.enum';
 import { RolesGuard } from '../auth/guards/roles.guard';
+import { MemberUpdate } from '../../libs/dto/member/member.update';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class MemberResolver {
@@ -25,13 +27,6 @@ export class MemberResolver {
     console.log('Mutation: login');
       return this.memberService.login(input); 
   }
-  
-  @UseGuards(AuthGuard)
-  @Mutation(() => String)
-  public async updateMember(@AuthMember("_id") memberId: ObjectId): Promise<string> {
-    console.log('Mutation: updateMember');
-     return this.memberService.updateMember();
-  }
 
   @UseGuards(AuthGuard)
   @Query(() => String)
@@ -47,15 +42,24 @@ export class MemberResolver {
   public async checkAuthRoles(@AuthMember() authMember: Member): Promise<string> {
     console.log('Query: checkAuthRoles');
     return `Hi ${authMember.memberNick}, you are ${authMember.memberType} (memberId: ${authMember._id})`;
-}
+  }
+  
+  @UseGuards(AuthGuard)
+  @Mutation(() => Member)
+  public async updateMember(
+    @Args('input') input: MemberUpdate,
+    @AuthMember("_id") memberId: ObjectId
+  ): Promise<Member> {
+    console.log('Mutation: updateMember');
+    delete input._id;
+     return this.memberService.updateMember(memberId, input);
+  }
 
-
-
-
-  @Query(() => String)
-  public async getMember(): Promise<string> {
+  @Query(() => Member)
+  public async getMember(@Args('memberId') input: string): Promise<Member> {
     console.log('Query: getMember');
-     return this.memberService.getMember();
+    const targetId = shapeIntoMongoObjectId(input);
+     return this.memberService.getMember(targetId);
   }
 
   /** ADMIN */
