a055fa66a5115d175f6356076a5d44e78da1d01a
Kenneth <avazqudratovv@gmail.com>
2026-08-29 02:04:09 -0700

    feat: develop getProperty business logic

--- apps/nestar-api/src/components/property/property.resolver.ts
+++ apps/nestar-api/src/components/property/property.resolver.ts
@@ -5,9 +5,12 @@ import { PropertyInput } from '../../libs/dto/property/property.input';
 import { MemberType } from '../../libs/enums/member.enum';
 import { Roles } from '../auth/decorators/roles.decorator';
 import { UseGuards } from '@nestjs/common';
+import { Query } from '@nestjs/graphql';
 import { RolesGuard } from '../auth/guards/roles.guard';
 import { AuthMember } from '../auth/decorators/authMember.decorator';
 import { ObjectId } from 'mongoose';
+import { WithoutGuard } from '../auth/guards/without.guard';
+import { shapeIntoMongoObjectId } from '../../libs/config';
 
 @Resolver()
 export class PropertyResolver {
@@ -23,6 +26,17 @@ export class PropertyResolver {
     console.log('Mutation: createProperty');
     input.memberId = memberId;
     return await this.propertyService.createProperty(input);
+  }
+
+  @UseGuards(WithoutGuard)
+  @Query((returns) => Property)
+  public async getProperty(
+    @Args('propertyId') input: string,
+    @AuthMember('_id') memberId
+  ): Promise<Property> {
+    console.log('Query: getProperty');
+    const propertyId = shapeIntoMongoObjectId(input);
+    return await this.propertyService.getProperty(memberId, propertyId);
   }
 }
 
