3b4c7f6ccfaea611551ebe9c5c1871b807949322
Kenneth <avazqudratovv@gmail.com>
2026-09-04 07:34:29 -0700

    fix: modify getMember business logic

--- apps/nestar-api/src/components/member/member.service.ts
+++ apps/nestar-api/src/components/member/member.service.ts
@@ -13,12 +13,15 @@ import { ViewGroup } from '../../libs/enums/view.enum';
 import { LikeInput } from '../../libs/dto/like/like.input';
 import { LikeGroup } from '../../libs/enums/like.enum';
 import { LikeService } from '../like/like.service';
+import { Follower, Following, MeFollowed } from '../../libs/dto/follow/follow';
 
 @Injectable()
 export class MemberService {
 	constructor(
 		@InjectModel('Member')
 		private readonly memberModel: Model<Member>,
+		@InjectModel('Follow')
+		private readonly followModel: Model<Follower | Following>,
 		private authService: AuthService,
 		private viewService: ViewService,
 		private likeService: LikeService,
@@ -94,10 +97,15 @@ export class MemberService {
 			
 			const likeInput = { memberId: memberId, likeRefId: targetId, likeGroup: LikeGroup.MEMBER };
 			targetMember.meLiked = await this.likeService.checkLikeExistence(likeInput);
-			// meFollowed
+			
+			targetMember.meFollowed = await this.checkSubscription(memberId, targetId);
 		}
-
 		return targetMember;
+	}
+
+	public async checkSubscription(followerId: ObjectId, followingId: ObjectId): Promise<MeFollowed[]> {
+    const result = await this.followModel.findOne({ followingId: followingId, followerId: followerId }).exec();
+    return result ? [{ followerId: followerId, followingId: followingId, myFollowing: true }] : [];
 	}
 
 	public async getAgents(memberId: ObjectId, input: AgentsInquiry): Promise<Members> {
@@ -178,7 +186,7 @@ export class MemberService {
 		return result;
 	}
 
-	  public async memberStatsEditor(input: StatisticModifier): Promise<Member> {
+	public async memberStatsEditor(input: StatisticModifier): Promise<Member> {
     const { _id, targetKey, modifier } = input;
     return await this.memberModel
       .findByIdAndUpdate(
